f2fa518013f462d88daeea27ffa608de9dafba6c
joonsooan <joonsooan@gmail.com>
2026-08-22 01:40:25 +0900

    [Task4-After] 책임 분리 리팩토링
    - GridMapManager.GetAllEntities 추가
    - FSM ProcessingPlayerTurn 상태-전이 구현
    - 턴 소모 판단을 PlayerController에서 TurnManager로 이전
    - ITurnActor 인터페이스 도입

--- Assets/Scripts/After/Entities/Enemy.cs
+++ Assets/Scripts/After/Entities/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour, IGridEntity, IDamageable
+public class Enemy : MonoBehaviour, IGridEntity, IDamageable, ITurnActor
 {
     [SerializeField] private EnemyDataSO enemyData;
 
