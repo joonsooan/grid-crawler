605bd921bfdd5e00ef79ed67f932d6935348ac81
joonsooan <joonsooan@gmail.com>
2026-08-21 18:36:42 +0900

    [Task3-After] 아이템 경로 차단 버그 수정 및 이동 로직 정리
    - Item을 entityMap 대신 별도 itemMap에 등록해 경로 탐색이 아이템에 막히지 않도록 수정
    - 플레이어의 moveRange는 턴 예산 의미로 분리 (TurnManager 연동은 TODO)

--- Assets/Scripts/After/Entities/Item.cs
+++ Assets/Scripts/After/Entities/Item.cs
@@ -1,22 +1,23 @@
 using UnityEngine;
 
-public class Item : MonoBehaviour, IGridEntity, IInteractable
+public class Item : MonoBehaviour, IInteractable
 {
     [SerializeField] private ItemDataSO itemData;
 
     public bool isOpened = false;
 
-    public Vector2Int GridPos { get; set; }
+    public Vector2Int GridPos { get; private set; }
 
     private void Start()
     {
-        ((IGridEntity)this).RegisterToGrid(transform.position);
+        GridPos = GridUtils.WorldToGrid(transform.position);
+        GridMapManager.Instance.RegisterItem(GridPos, this);
         transform.position = GridUtils.GridToWorld(GridPos, 0f);
     }
 
     private void OnDestroy()
     {
-        ((IGridEntity)this).UnregisterFromGrid();
+        GridMapManager.Instance.UnregisterItem(GridPos);
     }
 
     public void Interact(GameObject interactor)
