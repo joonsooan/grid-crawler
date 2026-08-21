ce694aa90af4586c1e1bfa6cbbbd0dcbd3fff2cc
joonsooan <joonsooan@gmail.com>
2026-08-21 18:36:42 +0900

    [Task3-After] 아이템 경로 차단 버그 수정 및 이동 로직 정리
    - Item을 entityMap 대신 별도 itemMap에 등록해 경로 탐색이 아이템에 막히지 않도록 수정
    - 플레이어의 moveRange는 턴 예산 의미로 분리 (TurnManager 연동은 TODO)

--- Assets/Scripts/After/Core/GridMapManager.cs
+++ Assets/Scripts/After/Core/GridMapManager.cs
@@ -14,6 +14,7 @@ public class GridMapManager : MonoBehaviour
     // O(1) 정적 타일 및 동적 엔티티 점유 상태 관리
     private Dictionary<Vector2Int, TileType> tileMap = new Dictionary<Vector2Int, TileType>();
     private Dictionary<Vector2Int, MonoBehaviour> entityMap = new Dictionary<Vector2Int, MonoBehaviour>();
+    private Dictionary<Vector2Int, Item> itemMap = new Dictionary<Vector2Int, Item>();
 
     private void Awake()
     {
@@ -63,5 +64,23 @@ public class GridMapManager : MonoBehaviour
     public bool TryGetEntity(Vector2Int pos, out MonoBehaviour entity)
     {
         return entityMap.TryGetValue(pos, out entity);
+    }
+
+    // 해당 칸에 아이템을 등록 (이동 차단 없이 상호작용만 가능)
+    public void RegisterItem(Vector2Int pos, Item item)
+    {
+        itemMap[pos] = item;
+    }
+
+    // 해당 칸의 아이템 등록을 해제
+    public void UnregisterItem(Vector2Int pos)
+    {
+        if (itemMap.ContainsKey(pos)) itemMap.Remove(pos);
+    }
+
+    // 특정 칸의 아이템을 O(1)로 조회
+    public bool TryGetItem(Vector2Int pos, out Item item)
+    {
+        return itemMap.TryGetValue(pos, out item);
     }
 }
