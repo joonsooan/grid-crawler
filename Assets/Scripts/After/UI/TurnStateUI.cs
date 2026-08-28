816837922d43cda8632fdd944686bcbe91462bb1
joonsooan <joonsooan@gmail.com>
2026-08-29 02:13:09 +0900

    [VFX] DOTween 기반 이동/전투/UI 연출 추가
    - 이동/공격/피격에 트윈 연출 추가
    - 아이템 스폰 팝업 + 호버 루프 추가
    - 체력, 턴 UI 연출 추가

--- Assets/Scripts/After/UI/TurnStateUI.cs
+++ Assets/Scripts/After/UI/TurnStateUI.cs
@@ -1,23 +1,53 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
 public class TurnStateUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text turnStateText;
+    [SerializeField] private float slideDistance = 100f;
+    [SerializeField] private float slideDuration = 0.3f;
+
+    private RectTransform rectTransform;
+    private Vector2 basePos;
+    private bool isEnemyTurnDisplayed;
+
+    private void Awake()
+    {
+        rectTransform = turnStateText.rectTransform;
+    }
 
     private void Start()
     {
+        basePos = rectTransform.anchoredPosition;
+
+        isEnemyTurnDisplayed = TurnManager.Instance.CurrentState == TurnState.ProcessingEnemyTurn;
+        turnStateText.text = isEnemyTurnDisplayed ? ">> 적 턴 <<" : ">> 플레이어 턴 <<";
+
         TurnManager.Instance.OnTurnStateChanged += Refresh;
-        Refresh(TurnManager.Instance.CurrentState);
     }
 
     private void OnDestroy()
     {
         TurnManager.Instance.OnTurnStateChanged -= Refresh;
+        rectTransform.DOKill();
     }
 
     private void Refresh(TurnState state)
     {
-        turnStateText.text = state == TurnState.ProcessingEnemyTurn ? ">> 적 턴 <<" : ">> 플레이어 턴 <<";
+        bool isEnemyTurn = state == TurnState.ProcessingEnemyTurn;
+        if (isEnemyTurn == isEnemyTurnDisplayed) return;
+        isEnemyTurnDisplayed = isEnemyTurn;
+
+        rectTransform.DOKill();
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(rectTransform.DOAnchorPosX(basePos.x - slideDistance, slideDuration * 0.5f).SetEase(Ease.InCubic));
+        sequence.AppendCallback(() =>
+        {
+            turnStateText.text = isEnemyTurn ? ">> 적 턴 <<" : ">> 플레이어 턴 <<";
+            rectTransform.anchoredPosition = new Vector2(basePos.x + slideDistance, basePos.y);
+        });
+        sequence.Append(rectTransform.DOAnchorPosX(basePos.x, slideDuration * 0.5f).SetEase(Ease.OutCubic));
     }
 }
