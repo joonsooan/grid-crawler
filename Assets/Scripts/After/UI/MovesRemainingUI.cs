54db495ed4759483d9347c1715706a9b5f4fb536
joonsooan <joonsooan@gmail.com>
2026-08-29 11:03:08 +0900

    [VFX] 게임오버, 픽업 연출 추가

--- Assets/Scripts/After/UI/MovesRemainingUI.cs
+++ Assets/Scripts/After/UI/MovesRemainingUI.cs
@@ -1,19 +1,24 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
 public class MovesRemainingUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text movesRemainingText;
+    [SerializeField] private float punchDuration = 0.25f;
+    [SerializeField] private float punchScaleAmount = 0.3f;
 
     private void Start()
     {
         TurnManager.Instance.OnMovesRemainingChanged += Refresh;
+        TurnManager.Instance.OnMovesIncreasedByItem += PlayPunch;
         Refresh(TurnManager.Instance.MovesRemaining, TurnManager.Instance.MaxMoves);
     }
 
     private void OnDestroy()
     {
         TurnManager.Instance.OnMovesRemainingChanged -= Refresh;
+        TurnManager.Instance.OnMovesIncreasedByItem -= PlayPunch;
     }
 
     private void Refresh(int current, int max)
@@ -25,5 +30,10 @@ public class MovesRemainingUI : MonoBehaviour
         }
 
         movesRemainingText.text = $"남은 이동 : {current} / {max}";
+    }
+
+    private void PlayPunch()
+    {
+        movesRemainingText.transform.DOPunchScale(Vector3.one * -punchScaleAmount, punchDuration, vibrato: 8);
     }
 }
